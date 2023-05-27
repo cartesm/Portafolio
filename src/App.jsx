c5d9f48cc04838acb1f9ebd160b36df189f37629
cartess <carteszero@gmail.com>
2023-05-27 13:45:13 -0400

    start with carrousel

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import {Carrousel,Header,Title,Description} from "./components/main"
 function App() {
 
   return (
-    <main className="bg-[#EDEEF0] max-w-[1920px] max-h-[1200px]">
+    <main className=" max-w-[1920px] max-h-[1200px]">
       <Header/>
       <Title/>
       <Description/>
--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -4,17 +4,17 @@ function Description() {
  
   return (
     <Reveal cascade>
-    <div className="grid grid-cols-1 -translate-y-26  md:grid-cols-2 place-items-center">
-        <div className="flex items-center my-11 md:my-0 flex-col gap-5">
+    <div className="flex flex-col md:flex-row pb-[100px] items-center justify-center gap-12">
+        <div className="flex items-center flex-col gap-5">
             <h3 className="font-sans text-4xl"><b>Acerca de mi</b></h3>
-            <p className=" xd w-[450px] md:w-[500px] px-6 font-sans text-lg indent-8 md:indent-7 leading-8">
+            <p className=" xd min-w-[450px] max-w-[500px] px-6 font-sans text-lg indent-8 md:indent-7 leading-8">
                 Soy un desarrollador web con amplia experiencia en HTML, CSS, JavaScript, React, MongoDB, Node.js y Express.
                 Además, tengo conocimientos en el mantenimiento de hardware, lo que me hace más versátil en el campo de la tecnología. 
                 En mi tiempo libre, me gusta leer, ver anime y películas, lo cual me ayuda a encontrar un equilibrio entre mi vida profesional y personal.
             </p>
         </div>
-        <div className="w-[370px] ">   
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="-11.5 -10.23174 23 20.46348">
+        <div className="">   
+          <svg className="min-w-[300px] max-w[400px]" xmlns="http://www.w3.org/2000/svg" viewBox="-11.5 -10.23174 23 20.46348">
             <circle cx="0" cy="0" r="2.05" fill="#61dafb"/>
             <g stroke="#61dafb"  fill="none">
               <ellipse rx="11" ry="4.2"/>
