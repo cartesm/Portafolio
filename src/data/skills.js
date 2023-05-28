ab5c402a9692416735f6b5f4bc2a0d26e7edc80a
cartess <carteszero@gmail.com>
2023-05-27 20:44:59 -0400

    finished 50% of  skills zone

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,8 +1,43 @@
 export const skills = [
     {
-        description:"",
-        exp:0
-    }
-        
+        tittle:"HTML5",    
+        description:"Lenguaje de marcado utilizado para estructurar y presentar el contenido en la web. Se utiliza para definir la estructura y los elementos de una página web.",
+        exp:3
+    },
+    {
+        tittle:"CSS",    
+        description:"Lenguaje de hojas de estilo utilizado para controlar la apariencia y el diseño de los elementos HTML en una página web. Permite definir el color, la fuente, el tamaño y la disposición de los elementos.",
+        exp:3
+    },
+      {
+        tittle:"JavaScript",    
+        description:"Lenguaje de programación utilizado para agregar interactividad y funcionalidad a las páginas web. Permite manipular elementos HTML, realizar operaciones matemáticas, interactuar con el usuario y acceder a datos en tiempo real.",
+        exp:3
+    },
+    {
+        tittle:"Tailwind CSS",    
+        description:"Framework de CSS que proporciona clases predefinidas para facilitar el diseño y el desarrollo web. Ofrece un enfoque de ´diseño utilitario´, donde se utilizan clases específicas para aplicar estilos directamente en los elementos.",
+        exp:3
+    },
+    {
+        tittle:"MongoDB",    
+        description:"Base de datos NoSQL orientada a documentos. Almacena los datos en forma de documentos flexibles similares a JSON, lo que permite una escalabilidad y flexibilidad fácilmente manejable.",
+        exp:2
+    },
+    {
+        tittle:"Express",    
+        description:"Framework web rápido y minimalista para Node.js. Permite crear aplicaciones web y APIs de manera sencilla, gestionando las rutas, el manejo de peticiones y respuestas, y proporcionando herramientas para trabajar con la base de datos.",
+        exp:1
+    },
+    {
+        tittle:"React",    
+        description:"Biblioteca de JavaScript para construir interfaces de usuario interactivas y reutilizables. Utiliza componentes reutilizables que se actualizan dinámicamente cuando cambian los datos, lo que facilita la creación de aplicaciones web rápidas y eficientes.",
+        exp:3
+    },
+    {
+        tittle:"Nodejs",    
+        description:"Entorno de ejecución de JavaScript del lado del servidor. Permite ejecutar código JavaScript fuera del navegador, lo que lo convierte en una opción popular para construir aplicaciones web y servicios del lado del servidor.",
+        exp:1
+    },  
 ]
 
